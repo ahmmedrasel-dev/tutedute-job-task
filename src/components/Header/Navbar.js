b2a743bde3dbd3e4656877f44593718049791ba8
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-11-13 01:48:13 +0600

    manu fix

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -12,7 +12,28 @@ const Navbar = () => {
 
         </div>
 
-        <div className="navbar-end mt-5 lg:mt-0">
+        <div className="navbar-end mt-5 hidden lg:block lg:mt-0">
+          <div className="navbar-center lg:flex">
+            <ul className="menu menu-horizontal p-0">
+              <li><a className='text-tuteSecondary'>My Assignment</a></li>
+              <li><a className='text-tuteSecondary'>Chat with Mentor</a></li>
+              <li tabIndex={0}>
+                <a className='text-tutePrimary'>
+                  <FaUserCircle />
+                  ProfileName
+                  <FaAngleDown />
+                </a>
+                <ul className="p-2 bg-base-100">
+                  <li><a >Profile</a></li>
+
+                  <li><a>Logout</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="navbar-end lg:hidden mt-5 lg:mt-0">
           <div className="navbar-center lg:flex">
             <ul className="menu menu-horizontal p-0">
               <li tabIndex={0}>
